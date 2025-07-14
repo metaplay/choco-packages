a2c507740eedc5d9e793392fa7661909aef86060
MetaplayBot <info@metaplay.io>
2025-07-14 14:09:01 +0000

    Add Chocolatey package for metaplay CLI v1.4.4

--- metaplay/tools/chocolateyinstall.ps1
+++ metaplay/tools/chocolateyinstall.ps1
@@ -8,8 +8,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
     packageName    = $packageName
     unzipLocation  = $toolsDir
-    url64bit       = 'https://github.com/metaplay/cli/releases/download/1.4.3/MetaplayCLI_Windows_x86_64.zip'
-    checksum64     = '45fb64ad22daa94c8332375300353be9a365e3d04afd48c06c12b4c2b73e7fc6'
+    url64bit       = 'https://github.com/metaplay/cli/releases/download/1.4.4/MetaplayCLI_Windows_x86_64.zip'
+    checksum64     = 'c393914e1105a1b4c477da0938059a501be4253342895048f34574636c7f6207'
     checksumType64 = 'sha256'
 }
 
